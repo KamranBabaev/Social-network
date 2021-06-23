2496e90f957a7c888ae759a2799b0d5d31adda0e
Kamran Babaev <babaevkamran2094@gmail.com>
2021-06-23 13:36:59 +0300

    clear for copy todolist

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,34 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/header/Header";
-import Navbar from "./components/navbar/Navbar";
-import Profile from "./components/profile/Profile";
-import Dialogs from "./components/dialogs/Dialogs";
-import {BrowserRouter, Route} from 'react-router-dom';
-import {addPost, stateType} from "./redux/state";
 
-export type AppPropsType = {
-    state: stateType;
-    addPost: (postMessage: string) => void
-}
-
-const App = (props: AppPropsType) => {
+const App = (props: any) => {
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path='/profile'
-                           render={() => <Profile
-                               posts={props.state.profilePage.posts} addPost={props.addPost}/>}/>
-                    <Route path='/dialogs'
-                           render={() => <Dialogs
-                               dialogs={props.state.messagePage.dialogs}
-                               messages={props.state.messagePage.messages}/>}/>
-                </div>
-            </div>
-        </BrowserRouter>
+        <div></div>
     )
 }
 
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import state from "./redux/state";
-import {addPost} from "./redux/state";
-import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App state={state} addPost={addPost}/>
-    </BrowserRouter>, document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
-
-reportWebVitals();
